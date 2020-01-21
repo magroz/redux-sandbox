c106990b79d1ae90244ffd3d31685bbc906a2903
Mike <magrozero@yandex.ru>
2020-01-21 16:55:23 +0300

    127

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-console.log('1');
+import App from './components/App';
+import reducer from "./reducer";
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root'));
